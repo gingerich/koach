b99ffbce2b955f2bbedceb9c2344fb3ee718a9ab
gingerich <marlin.gingerich@gmail.com>
2017-08-31 02:14:54 -0400

    fix exports

--- src/index.js
+++ src/index.js
@@ -1,38 +1,20 @@
 import Koach from './lib/Koach'
 
-/*
- * Exports
-*/
-export { default as Component } from './components/Component'
-export { default as Controller } from './components/Controller'
-export { default as Module } from './components/Module'
-export { default as Mount } from './components/Mount'
-export { default as Router } from './components/Router'
-export { default as Stack } from './components/Stack'
-
-export { default as Server, NodeServer, listener } from './lib/Server'
-export { default as KoaServer } from './lib/KoaServer'
-export { default as Spec } from './lib/Spec'
-
-export { Koach as Provider }
-
-/*
- * Backwards compatability
-*/
 import Component from './components/Component'
 import Controller from './components/Controller'
 import Module from './components/Module'
 import Mount from './components/Mount'
-import Router from './components/Router'
+import * as Router from './components/Router'
 import Stack from './components/Stack'
-import Server from './lib/Server'
+import * as Server from './lib/Server'
 import KoaServer from './lib/KoaServer'
 import Spec from './lib/Spec'
 
-const moduleExports = {
+exports = module.exports = new Koach()
+Object.assign(module.exports, {
+  Provider: Koach,
   Component,
   Controller,
-  Provider: Koach,
   KoaServer,
   Module,
   Mount,
@@ -40,6 +22,4 @@ const moduleExports = {
   Server,
   Spec,
   Stack
-}
-
-export default Object.assign(new Koach(), moduleExports)
+})
