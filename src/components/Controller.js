0f9f2de4761411b0bd51af580ae2ea748169e417
gingerich <marlin.gingerich@gmail.com>
2017-09-05 00:03:45 -0400

    controller debug logging

--- src/components/Controller.js
+++ src/components/Controller.js
@@ -2,7 +2,7 @@ import methods from 'methods'
 
 import Component from './Component'
 
-const debug = require('debug')('koach')
+const debug = require('debug')('koach:controller')
 const route = require('koa-path-match')()
 
 // @path('/:id')
@@ -11,15 +11,18 @@ module.exports = class Controller extends Component {
     const controller = this.config.controller || this
     const path = this.constructor.PATH || this.config.path || '/'
     const router = route(path)
+    const handledMethods = []
     methods.forEach((method) => {
       const handler = controller[method]
       if (handler) {
         if (typeof (handler) !== 'function') {
           throw new Error(`Expected controller.${method} to be a function`)
         }
+        handledMethods.push(method)
         router[method](ctx => handler.call(controller, ctx))
       }
     })
+    debug(`[${handledMethods}] ${path}`)
     return router
   }
 }
